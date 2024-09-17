647d0f3e519c18703f5d4159a161a3506118b7b5
KINGdeusX <warcommelite@gmail.com>
2024-09-17 23:05:35 +0800

    Stepper Motor Code Structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,34 @@
 // DRV8825_CLOCK_WISE         = 0;  //  LOW
 // DRV8825_COUNTERCLOCK_WISE  = 1;  //  HIGH
 
-
-
-DRV8825 stepper; // Defined Stepper
+DRV8825 stepper1, stepper2, stepper3, stepper4; // Defined Stepper
+int stepper_internal_delay = 10; // 10 ms for every step
+int stppr_wingL_num_steps = 300; // Number of steps defined for the Wing left Extractor
+int stppr_WingR_num_steps = 300; // Number of steps defined for the Wing right Extractor
+int stppr_cntrl_gate = 200; // Control Gate number fo steps
+int stppr_trash_puller = 400; // Trash net puller
  
 void setup() {
 	// Code Here
 	Serial.begin(115200);
-	stepper.begin(5, 4);  // direction + step pin
-  	stepper.setDirection(DRV8825_CLOCK_WISE);
+	stepper1.begin(5, 4);
+	// stepper2.begin(5, 4);
+	// stepper3.begin(5, 4);
+	// stepper4.begin(5, 4);
 }
+
+void wingL(const boolean direction) {
+  	stepper1.setDirection(direction); // Direction
+	for(int i = 0; i <= stppr_wingL_num_steps; i++){
+		stepper1.step(); // Execute Step
+		delay(stepper_internal_delay); 
+	}
+}
+// void wingR() {}
+// void Cntrl() {}
+// void Puller() {}
  
 void loop() {
-	for(int i = 0; i < 300; i++){
-		stepper.step();
-		delay(10000);
-	}
+	wingL(0);
+	delay(10000);
 }
