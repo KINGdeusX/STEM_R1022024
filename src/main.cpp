0f3ef1279af4870cb0d3ee888e267b3d87a6c425
KINGdeusX <warcommelite@gmail.com>
2024-09-18 23:19:40 +0800

    updated the source code

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 #define STEPPER_2_S 12
 #define STEPPER_3_D 13
 #define STEPPER_3_S 15
-#define laser1 10
 #define sensor1 9
 
 // setDirection
@@ -18,10 +17,16 @@
 
 DRV8825 stepper1, stepper2, stepper3, stepper4; // Defined Stepper
 int stepper_internal_delay = 10; // 10 ms for every step
+int execution_delay = 1000;
+int discarding_delay = 5000;
+
 int stppr_wingL_num_steps = 300; // Number of steps defined for the Wing left Extractor
 int stppr_WingR_num_steps = 300; // Number of steps defined for the Wing right Extractor
 int stppr_cntrl_gate = 200; // Control Gate number fo steps
 int stppr_trash_puller = 400; // Trash net puller
+
+bool mode = 0; // 0 Automatic 1 Manual
+bool exec = 0; // 0 noting happens 1 sequence begins
  
 void setup() {
 	// Code Here
@@ -30,6 +35,7 @@ void setup() {
 	stepper2.begin(STEPPER_1_D, STEPPER_1_S);
 	stepper3.begin(STEPPER_2_D, STEPPER_2_S);
 	stepper4.begin(STEPPER_3_D, STEPPER_3_S);
+	pinMode(sensor1, INPUT);
 }
 
 void wingL(const boolean direction, int steps) {
@@ -39,19 +45,80 @@ void wingL(const boolean direction, int steps) {
 		delay(stepper_internal_delay); 
 	}
 }
-// void wingR() {}
-// void Cntrl() {}
-// void Puller() {}
+void wingR(const boolean direction, int steps) {
+	stepper2.setDirection(direction); // Direction
+	for(int i = 0; i <= steps; i++){
+		stepper2.step(); // Execute Step
+		delay(stepper_internal_delay); 
+	}
+}
+void Cntrl(const boolean direction, int steps) {
+	stepper3.setDirection(direction); // Direction
+	for(int i = 0; i <= steps; i++) {
+		stepper3.step(); // Execute Step
+		delay(stepper_internal_delay); 
+	}
+}
+void Puller(const boolean direction, int steps) {
+	stepper4.setDirection(direction); // Direction
+	for(int i = 0; i <= steps; i++){
+		stepper4.step(); // Execute Step
+		delay(stepper_internal_delay);
+	}
+}
  
 void loop() {
-	//MOD
-	digitalWrite(laser1, HIGH);
-	bool value = digitalRead(sensor1);
+	//Code Here
+	bool sensor_value = digitalRead(sensor1); // Waits for the laser data
 
-	if (value) {
-		Serial.println("Laser Detected");
+	if (sensor_value == true) {
+		exec = 1;
+		Serial.println("Laser Obstructed");
 	}
 	else {
-		Serial.println("NO Laser");
+		exec = 0;
+		Serial.println("Laser Clear");
+	}
+
+	if (exec == 0) {
+		// Standby
+	}
+	else if (exec == 1) {
+		Serial.println("Executing");
+
+		Serial.println("Raising Control Gate");
+		Cntrl(0, stppr_cntrl_gate); // Raise the control gate
+		Serial.println("Control Gate Raised");
+		delay(execution_delay);
+
+		Serial.println("Rolling Trash");
+		Puller(0, stppr_trash_puller); // Rolls up the trash
+		Serial.println("Trash Rolled");
+		delay(execution_delay);
+		
+		Serial.println("Raising The Extractor");
+		wingL(0, stppr_wingL_num_steps);
+		Serial.println("Extractor Raised");
+		delay(execution_delay);
+
+		delay(discarding_delay); // delay for discarding the trash
+		
+		Serial.println("Lowering the Extractor");
+		wingL(1, stppr_wingL_num_steps);
+		Serial.println("Extractor Lowered");
+		delay(execution_delay);
+		
+		Serial.println("Unrolling Trash");
+		Puller(1, stppr_trash_puller); // Rolls up the trash
+		Serial.println("Trash unrolled");
+		delay(execution_delay);
+
+		Serial.println("Lowering Control Gate");
+		Cntrl(1, stppr_cntrl_gate); // Raise the control gate
+		Serial.println("Control Gate Lowered");
+		delay(execution_delay);
+
+		Serial.println("Execution Finished");
+		delay(5000); // Leveling Delay
 	}
 }
